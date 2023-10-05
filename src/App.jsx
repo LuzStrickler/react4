1172a097fbd3736c0345ef33dd216b9d5d0d89ae
Luz <luzstrickler@iresm.edu.ar>
2023-10-05 10:06:14 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <>
+    export default function Bio() 
+  return (
+  <div className="intro">
+  <h1>Welcome to my website!</h1>
+  <p className="summary">
+You can find my thoughts here.
+<br />
+<br />
+<b>And <i>pictures</i> of scientists!</b>
+</p>
+</div>
+);
+
+    </>
+  )
+}
+
+export default App
